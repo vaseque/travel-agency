679b83bf96b0399cd68fb9435e84052ecbf49152
artur wasek <vaseque@yahoo.co.uk>
2020-07-19 21:54:20 +0200

    Added first set of tests jest/enzyme v3

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -25,13 +25,13 @@ const TripSummary = ({id, image, name, cost, days, tags}) => (
 );
 
 TripSummary.propTypes = {
-  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  image: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  image: PropTypes.string,
+  name: PropTypes.string,
   intro: PropTypes.string,
-  cost: PropTypes.string.isRequired,
-  days: PropTypes.number.isRequired,
-  tags: PropTypes.array.isRequired,
+  cost: PropTypes.string,
+  days: PropTypes.number,
+  tags: PropTypes.array,
 };
 
 export default TripSummary;
--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -5,44 +5,61 @@ import {Link} from 'react-router-dom';
 
 describe('Component TripSummary', () => {
 
+  let component;
+  beforeEach(() => {
+    const id = 'abc';
+    const img= 'img.jpg';
+    const name = 'xyz';
+    const cost = '123';
+    const days = 123;
+    const tags = ['tag1', 'tag2', 'tag3'];
+
+    component = shallow(<TripSummary
+      id={id}
+      image={img} 
+      name={name}
+      cost={cost} 
+      days={days}
+      tags={tags}
+    />);
+  });
+
   it('should create correct link address', () => {
     const expectedId = 'abc';
-    const component = shallow(<TripSummary id={expectedId} />);
-    expect(component.find(Link).props('to')).toEqual(`/trip/${expectedId}`);
-  });
-
-  it('should check img got correct props', () => {
-    const expectedSrc = 'img.jpg';
-    const expectedAlt = 'imgDesc';
-    const component = shallow(<TripSummary img={expectedSrc} name={expectedAlt} />);
-    expect(component.find('img').prop('src')).toEqual(expectedSrc);
-    expect(component.find('img').prop('alt')).toEqual(expectedSrc);
-  });
-
-  it('should correctly render props: name cost days', () => {
-    const expectedName = 'abc';
-    const expectedCost = '123';
-    const expectedDays = 123;
-    const component = shallow(<TripSummary name={expectedName} cost={expectedCost} days={expectedDays} />);
-    expect(component.find('.title').text()).toEqual(expectedName);
-    expect(component.find('.details span').at(1).text()).toEqual('from' + ' ' + expectedName);
-    expect(component.find('.details span').at(0).text()).toEqual(expectedName + ' ' + 'days');
+    expect(component.find(Link).prop('to')).toEqual(`/trip/${expectedId}`);
   });
   
+  it('should check img got correct props', () => {
+    const expectedImg = 'img.jpg';
+    const expectedName = 'xyz';
+    expect(component.find('img').prop('src')).toEqual(expectedImg);
+    expect(component.find('img').prop('alt')).toEqual(expectedName);
+  });
+  
+  it('should correctly render props: name cost days', () => {
+    const expectedName = 'xyz';
+    const expectedCost = '123';
+    const expectedDays = 123;
+    expect(component.find('.title').text()).toEqual(expectedName);
+    expect(component.find('.details span').at(0).text()).toEqual(expectedDays + ' ' + 'days');
+    expect(component.find('.details span').at(1).text()).toEqual('from' + ' ' + expectedCost);
+  });
+    
   it('should throw error without required props', () => {
     expect(() => shallow(<TripSummary />)).toThrow();
   });
-
+  
   it('should render given tags through props from array in consecutive order', () => {
     const expectedTags = ['tag1', 'tag2', 'tag3'];
-    const component = shallow(<TripSummary tags={expectedTags} />);
-    expect(component.find('.tags span').at(0)).toEqual(expectedTags[0]);
-    expect(component.find('.tags span').at(1)).toEqual(expectedTags[1]);
-    expect(component.find('.tags span').at(2)).toEqual(expectedTags[2]);
+    expect(component.find('.tags span').at(0).text()).toEqual(expectedTags[0]);
+    expect(component.find('.tags span').at(1).text()).toEqual(expectedTags[1]);
+    expect(component.find('.tags span').at(2).text()).toEqual(expectedTags[2]);
   });
 
   it('it should not render parent element for nesting tag spans if prop tags falsy', () => {
-    const component = shallow(<TripSummary tags={null} />);
-    expect(component.find('.tags')).toHaveLength(0);
+    const tags = [];
+    const component = shallow(<TripSummary tags={tags} />);
+    expect(component.find('.tags span')).toHaveLength(0);
   });
+  
 });
